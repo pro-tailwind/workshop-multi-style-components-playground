3dacd48ce4245a3119f1605448fff86e8c16afb0
Simon Vrachliotis <simon@simonswiss.com>
2022-09-15 09:27:08 +1000

    Starting point step 3

--- src/components/button/react.tsx
+++ src/components/button/react.tsx
@@ -7,22 +7,33 @@ import { cx } from '../../utils'
 const baseClasses =
   'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 active:translate-y-px disabled:pointer-events-none disabled:opacity-50'
 
+const impactClasses = {
+  bold: 'bg-indigo-500 text-white shadow-md hover:bg-indigo-600',
+  light: 'bg-indigo-100 text-indigo-700 hover:bg-indigo-200',
+  none: 'bg-transparent text-indigo-700 hover:bg-indigo-50',
+}
+
 /*
   ------------------------------
-  1. Populate the `impactClasses` lookup object below.
-  This object should have a key for each possible `impact` value,
-  and the key should mirror the prop value.
+  1. Populate the `sizeClasses` and `shapeClasses` lookup objects below, 
+  following the same approach we used for the `impactClasses` object.
   ------------------------------
 */
-const impactClasses = {}
+const sizeClasses = {}
+const shapeClasses = {}
 
+// ------------------------------
 // Possible prop values for `size`, `impact` and `shape`
+// ------------------------------
 interface ButtonProps extends React.ComponentProps<'button'> {
   size?: 'small' | 'medium' | 'large'
   impact?: 'bold' | 'light' | 'none'
   shape?: 'square' | 'rounded' | 'pill'
 }
 
+// ------------------------------
+// Component definition (with default variants)
+// ------------------------------
 const Button = ({
   size = 'medium',
   impact = 'bold',
@@ -34,13 +45,11 @@ const Button = ({
       {...restProps}
       /* 
         ------------------------------
-        2. Add the appropriate `impactClasses` values to the 
+        2. Add the appropriate `sizeClasses` and `shapeClasses` values to the 
         className attribute below.
-        The `cx()` function imported at the top will merge
-        a series of comma separated inputs!
         ------------------------------
       */
-      classname={baseClasses}
+      className={cx(baseClasses, impactClasses[impact])}
     />
   )
 }
