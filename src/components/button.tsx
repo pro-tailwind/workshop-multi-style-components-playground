41667440e45cae8108dfb6e9a04785ced7081c82
Simon Vrachliotis <simon@simonswiss.com>
2022-09-19 11:31:56 +1000

    Initial, non accesible modal setup

---
+++ src/components/button.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { cx } from '../utils'
+
+// ------------------------------
+// Tailwind Classes lookup directory
+// ------------------------------
+const baseClasses =
+  'font-semibold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 active:translate-y-px disabled:pointer-events-none disabled:opacity-50'
+
+const impactClasses = {
+  bold: 'bg-indigo-500 text-white shadow-md hover:bg-indigo-600',
+  light: 'bg-indigo-100 text-indigo-700 hover:bg-indigo-200',
+  none: 'bg-transparent text-indigo-700 hover:bg-indigo-50',
+}
+
+/*
+  ------------------------------
+  1. Populate the `sizeClasses` and `shapeClasses` lookup objects below, 
+  following the same approach we used for the `impactClasses` object.
+  ------------------------------
+*/
+const sizeClasses = {
+  small: 'px-3 py-1 text-sm',
+  medium: 'px-5 py-2',
+  large: 'px-7 py-2.5 text-lg',
+}
+const shapeClasses = {
+  square: '',
+  rounded: 'rounded',
+  pill: 'rounded-full',
+}
+
+// ------------------------------
+// Possible prop values for `size`, `impact` and `shape`
+// ------------------------------
+interface ButtonProps extends React.ComponentProps<'button'> {
+  size?: 'small' | 'medium' | 'large'
+  impact?: 'bold' | 'light' | 'none'
+  shape?: 'square' | 'rounded' | 'pill'
+}
+
+// ------------------------------
+// Component definition (with default variants)
+// ------------------------------
+const Button = ({
+  size = 'medium',
+  impact = 'bold',
+  shape = 'rounded',
+  ...restProps
+}: ButtonProps) => {
+  return (
+    <button
+      {...restProps}
+      /* 
+        ------------------------------
+        2. Add the appropriate `sizeClasses` and `shapeClasses` values to the 
+        className attribute below.
+        ------------------------------
+      */
+      className={cx(baseClasses, impactClasses[impact], sizeClasses[size], shapeClasses[shape])}
+    />
+  )
+}
+
+export default Button
---
+++ src/components/modal.jsx
@@ -0,0 +1,56 @@
+import * as React from 'react'
+
+import Button from './button'
+
+export default function Modal() {
+  const [isOpen, setIsOpen] = React.useState(true)
+
+  return (
+    <div>
+      <Button impact="light" onClick={() => setIsOpen(true)}>
+        Toggle modal
+      </Button>
+      <div className="relative z-10">
+        {isOpen && (
+          <>
+            {/* Overlay */}
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+            {/* Dialog */}
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="text-center sm:text-left">
+                      <h3
+                        className="text-xl font-semibold leading-6 text-slate-900"
+                        id="modal-title"
+                      >
+                        Confirm Subscription
+                      </h3>
+                      <div className="mt-4">
+                        <p className="text-slate-500">
+                          You're about to confirm your{' '}
+                          <a class="underline text-indigo-600 hover:text-indigo-500" href="#">
+                            membership subscription
+                          </a>
+                          . Your account will be billed for a one-year membership.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="border-t gap-2 p-4 flex flex-col sm:flex-row-reverse">
+                    <Button onClick={() => setIsOpen(false)}>Yes, confirm</Button>
+                    <Button impact="none" onClick={() => setIsOpen(false)}>
+                      Cancel
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
