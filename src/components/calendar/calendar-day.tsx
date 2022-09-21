9f6bdd755b4e484afe64ebd20e6e709750f8c447
Simon Vrachliotis <simon@simonswiss.com>
2022-09-22 09:29:16 +1000

    Types, better comments, availability status table

--- src/components/calendar/calendar-day.tsx
+++ src/components/calendar/calendar-day.tsx
@@ -1,71 +1,128 @@
-import React from 'react'
+/* 
+  The first few lines are wiring up React-Aria's `useCalendar` implementation. 
+  You don't need to worry about any of that for this challenge. 
+  
+  If curious, you can learn more about the `useCalendar` hook here: 
+  https://react-spectrum.adobe.com/react-aria/useCalendar.html
+*/
+import * as React from 'react'
 import { useCalendarCell } from 'react-aria'
-import { isSameDay, parseDateTime, isToday } from '@internationalized/date'
+import { isSameDay, parseDateTime, isToday, getLocalTimeZone } from '@internationalized/date'
+
 import { cx } from '../../utils'
 
+// ------------------------------
+// Component definition
+// ------------------------------
 export default function CalendarDay({ state, date, bookingAvailabilities }) {
-  // React-Aria stuff
   const ref = React.useRef()
   const { cellProps, buttonProps, isSelected, isOutsideVisibleRange, isDisabled, formattedDate } =
     useCalendarCell({ date }, state, ref)
 
-  // Check if any given day has some availability
+  // Checking if the day is today
+  const localTimezone = getLocalTimeZone()
+  const isCurrentDay = isToday(date, localTimezone)
+
+  /* 
+    Our calendar has certain available, "bookable" dates.
+    We create a list of days with availability, so that
+    we can style those calendar days differently.
+  */
   const hasAvailability = bookingAvailabilities.some((availability) =>
     isSameDay(parseDateTime(availability.startTime), date)
   )
 
+  /* 
+    ---------------------
+    ------------------------
+    ---------------------------
+    ------------------------------
+    === WORKSHOP CHALLENGE ===
+    All our work is happening 
+    below this comment.
+    ------------------------------
+    ---------------------------
+    ------------------------
+    ---------------------
+  */
+
+  /* 
+    We've identified that a calendar day will always be in 
+    one of these states, or "availability statuses",
+    represented by the `Status` type:
+  */
+  type Status = 'DISABLED' | 'SELECTED' | 'NO_VACANCY' | 'TODAY_NO_VACANCY' | 'VACANCY'
+
   // ------------------------------
-  // Styles lookup
+  // Styles lookup dictionary
   // ------------------------------
 
+  // Common classes to all statuses
   const baseClasses =
     'relative w-12 max-w-full aspect-square rounded-full grid place-items-center focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-400'
 
-  // Defining an Enum of the possible availability statuses
-  type Status = 'DISABLED' | 'NO_VACANCY' | 'TODAY_NO_VACANCY' | 'VACANCY' | 'SELECTED'
-
+  // Status-specific classes
   const statusClasses: Record<Status, string> = {
-    DISABLED: 'text-gray-300 pointer-events-none',
-    NO_VACANCY: 'text-gray-900 hover:bg-gray-100',
-    TODAY_NO_VACANCY: 'text-indigo-600 font-bold hover:bg-gray-100',
-    VACANCY: 'bg-indigo-100 text-indigo-600 font-bold hover:bg-indigo-200',
+    DISABLED: 'text-slate-300 pointer-events-none',
     SELECTED: 'bg-indigo-600 text-white font-bold bg-stripes',
+    NO_VACANCY: 'text-slate-900 hover:bg-slate-100',
+    TODAY_NO_VACANCY: 'text-indigo-600 font-bold hover:bg-slate-100',
+    VACANCY: 'bg-indigo-100 text-indigo-600 font-bold hover:bg-indigo-200',
   }
 
-  // Helper to gather the actual status
-  const getAvailibilityStatus: () => {} = () => {
-    return 'DISABLED'
+  /* 
+    We need to determine _what_ status the day is actually in.
+    In this case, there is a bit more complexity to it than 
+    what our simple lookup object can support. We need 
+    some conditional logic. Let's write a function 
+    to handle all the possible scenarios.
+  */
+  type GetStatus = () => Status
+
+  const getAvailibilityStatus: GetStatus = function () {
     if (isDisabled) return 'DISABLED'
     if (isSelected) return 'SELECTED'
     if (!hasAvailability) {
-      return isToday(date, 'Australia/Sydney') ? 'TODAY_NO_VACANCY' : 'NO_VACANCY'
+      return isCurrentDay ? 'TODAY_NO_VACANCY' : 'NO_VACANCY'
     }
     return 'VACANCY'
   }
 
   return (
+    // Spreading `useCalendar` props on the various elememnts...
     <td {...cellProps}>
       <div
         {...buttonProps}
         ref={ref}
         hidden={isOutsideVisibleRange}
+        /* 
+          The className attribute below is where 
+          we compose our styles back together.
+        */
         className={cx(
           baseClasses,
           statusClasses[getAvailibilityStatus()]
+
+          /*
+            Below is the "before" implementation, just for comparison.
+            I think we can agree our new logic is easier to follow!
+          */
+
           // calendarClasses.base,
           // isSelected && calendarClasses.selected,
           // isDisabled && calendarClasses.disabled,
-          // isToday(date) && !isSelected && calendarClasses.today,
+          // isCurrentDay && !isSelected && calendarClasses.today,
           // hasAvailability && !isDisabled && !isSelected && calendarClasses.hasAvailability,
           // !hasAvailability &&
-          //   !isToday(date) &&
+          //   !isCurrentDay &&
           //   !isDisabled &&
           //   !isSelected &&
           //   calendarClasses.candidate
         )}
       >
         <span>{formattedDate}</span>
-        {isToday(date, 'Australia/Sydney') && (
+        {/* Adding a "spot" below the current day */}
+        {isCurrentDay && (
           <span
             className={cx(
               'absolute bottom-2 left-1/2 h-1 w-1 -translate-x-1/2 rounded-full',
--- src/components/reference/calendar-day.tsx
+++ src/components/reference/calendar-day.tsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import * as React from 'react'
 import { useCalendarCell } from 'react-aria'
-import { isSameDay, parseDateTime, isToday } from '@internationalized/date'
-import cx from 'classnames'
+import { isSameDay, parseDateTime, isToday, getLocalTimeZone } from '@internationalized/date'
 
+import { cx } from '../../utils'
+
+// ------------------------------
+// Component definition
+// ------------------------------
 export default function CalendarDay({ state, date, bookingAvailabilities }) {
-  // React-Aria stuff
   const ref = React.useRef()
   const { cellProps, buttonProps, isSelected, isOutsideVisibleRange, isDisabled, formattedDate } =
     useCalendarCell({ date }, state, ref)
 
-  // Check if any given day has some availability
+  // Checking if the day is today
+  const localTimezone = getLocalTimeZone()
+  const isCurrentDay = isToday(date, localTimezone)
+
+  /* 
+    Our calendar has certain available, "bookable" dates.
+    We create a list of days with availability, so that
+    we can style those calendar days differently.
+  */
   const hasAvailability = bookingAvailabilities.some((availability) =>
     isSameDay(parseDateTime(availability.startTime), date)
   )
 
-  const isCurrentDay = isToday(date)
-
-  // Tailwind classes lookup
+  // ------------------------------
+  // Styles lookup dictionary
+  // ------------------------------
   const calendarClasses = {
     base: 'relative w-12 max-w-full aspect-square rounded-full grid place-items-center focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-400',
     selected: 'bg-indigo-600 text-white font-bold bg-stripes',
-    disabled: 'text-gray-300 pointer-events-none',
-    today: 'text-indigo-600 font-bold',
+    disabled: 'text-slate-300 pointer-events-none',
+    today: 'text-indigo-600 font-bold hover:bg-slate-100',
     hasAvailability: 'bg-indigo-100 text-indigo-600 font-bold hover:bg-indigo-200',
-    candidate: 'hover:bg-gray-100 text-gray-900',
+    candidate: 'hover:bg-slate-100 text-slate-900',
   }
 
   return (
+    // Spreading `useCalendar` props on the various elememnts...
     <td {...cellProps}>
       <div
         {...buttonProps}
@@ -36,17 +48,18 @@ export default function CalendarDay({ state, date, bookingAvailabilities }) {
           calendarClasses.base,
           isSelected && calendarClasses.selected,
           isDisabled && calendarClasses.disabled,
-          isToday(date) && !isSelected && calendarClasses.today,
+          isCurrentDay && !isSelected && calendarClasses.today,
           hasAvailability && !isDisabled && !isSelected && calendarClasses.hasAvailability,
           !hasAvailability &&
-            !isToday(date) &&
+            !isCurrentDay &&
             !isDisabled &&
             !isSelected &&
             calendarClasses.candidate
         )}
       >
         <span>{formattedDate}</span>
-        {isToday(date) && (
+        {/* Adding a "spot" below the current day */}
+        {isCurrentDay && (
           <span
             className={cx(
               'absolute bottom-2 left-1/2 h-1 w-1 -translate-x-1/2 rounded-full',
