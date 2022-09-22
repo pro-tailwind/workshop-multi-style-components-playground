df667fcdf943611baba2ae3a6f317aa2f635fcec
Simon Vrachliotis <simon@simonswiss.com>
2022-09-22 12:11:06 +1000

    Challenge starting point

--- src/components/calendar/calendar-day.tsx
+++ src/components/calendar/calendar-day.tsx
@@ -5,11 +5,11 @@
   If curious, you can learn more about the `useCalendar` hook here: 
   https://react-spectrum.adobe.com/react-aria/useCalendar.html
 */
+
 import * as React from 'react'
 import { useCalendarCell } from 'react-aria'
 import { isSameDay, parseDateTime, isToday, getLocalTimeZone } from '@internationalized/date'
 
-import { Status, baseClasses, statusClasses } from './availability-status'
 import { cx } from '../../utils'
 
 // ------------------------------
@@ -33,36 +33,18 @@ export default function CalendarDay({ state, date, bookingAvailabilities }) {
     isSameDay(parseDateTime(availability.startTime), date)
   )
 
-  /* 
-    ---------------------
-    ------------------------
-    ---------------------------
-    ------------------------------
-    === WORKSHOP CHALLENGE ===
-    All our work is happening 
-    below this comment.
-    ------------------------------
-    ---------------------------
-    ------------------------
-    ---------------------
-  */
+  // ------------------------------
+  // Styles lookup dictionary
+  // ------------------------------
+  const baseClasses =
+    'relative w-12 max-w-full aspect-square rounded-full grid place-items-center focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-400'
 
-  /* 
-    We need to determine _what_ status the day is actually in.
-    In this case, there is a bit more complexity to it than 
-    what our simple lookup object can support. We need 
-    some conditional logic. Let's write a function 
-    to handle all the possible scenarios.
-  */
-  type GetStatus = () => Status
-
-  const getAvailibilityStatus: GetStatus = function () {
-    if (isDisabled) return 'DISABLED'
-    if (isSelected) return 'SELECTED'
-    if (!hasAvailability) {
-      return isCurrentDay ? 'TODAY_NO_VACANCY' : 'NO_VACANCY'
-    }
-    return 'VACANCY'
+  const dynamicClasses = {
+    disabled: 'text-slate-300 pointer-events-none',
+    candidate: 'hover:bg-slate-100 text-slate-900',
+    hasAvailability: 'bg-indigo-100 text-indigo-600 font-bold hover:bg-indigo-200',
+    today: 'text-indigo-600 font-bold hover:bg-slate-100',
+    selected: 'bg-indigo-600 text-white font-bold bg-stripes',
   }
 
   return (
@@ -72,28 +54,17 @@ export default function CalendarDay({ state, date, bookingAvailabilities }) {
         {...buttonProps}
         ref={ref}
         hidden={isOutsideVisibleRange}
-        /* 
-          The className attribute below is where 
-          we compose our styles back together.
-        */
         className={cx(
           baseClasses,
-          statusClasses[getAvailibilityStatus()]
-
-          /*
-            Below is the "before" implementation, just for comparison.
-            I think we can agree our new logic is easier to follow!
-          */
-
-          // isSelected && dynamicClasses.selected,
-          // isDisabled && dynamicClasses.disabled,
-          // isCurrentDay && !isSelected && dynamicClasses.today,
-          // hasAvailability && !isDisabled && !isSelected && dynamicClasses.hasAvailability,
-          // !hasAvailability &&
-          //   !isCurrentDay &&
-          //   !isDisabled &&
-          //   !isSelected &&
-          //   dynamicClasses.candidate
+          isSelected && dynamicClasses.selected,
+          isDisabled && dynamicClasses.disabled,
+          isCurrentDay && !isSelected && dynamicClasses.today,
+          hasAvailability && !isDisabled && !isSelected && dynamicClasses.hasAvailability,
+          !hasAvailability &&
+            !isCurrentDay &&
+            !isDisabled &&
+            !isSelected &&
+            dynamicClasses.candidate
         )}
       >
         <span>{formattedDate}</span>
