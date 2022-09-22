253786a509a6577fa1bd4a94016aa93427c893b9
Simon Vrachliotis <simon@simonswiss.com>
2022-09-22 15:23:47 +1000

    End of step 3

--- src/components/calendar/calendar-day.tsx
+++ src/components/calendar/calendar-day.tsx
@@ -1,10 +1,3 @@
-/* 
-  The first few lines are wiring up React-Aria's `useCalendar` implementation. 
-  You don't need to worry about any of that for this challenge. 
-  
-  If curious, you can learn more about the `useCalendar` hook here: 
-  https://react-spectrum.adobe.com/react-aria/useCalendar.html
-*/
 import * as React from 'react'
 import { useCalendarCell } from 'react-aria'
 import { isSameDay, parseDateTime, isToday, getLocalTimeZone } from '@internationalized/date'
@@ -23,11 +16,7 @@ export default function CalendarDay({ state, date, bookingAvailabilities }) {
   const localTimezone = getLocalTimeZone()
   const isCurrentDay = isToday(date, localTimezone)
 
-  /* 
-    Our calendar has certain available, "bookable" dates.
-    We create a list of days with availability, so that
-    we can style those calendar days differently.
-  */
+  // Days with booking availability
   const hasAvailability = bookingAvailabilities.some((availability) =>
     isSameDay(parseDateTime(availability.startTime), date)
   )
@@ -37,13 +26,6 @@ export default function CalendarDay({ state, date, bookingAvailabilities }) {
   // ------------------------------
   const baseClasses =
     'relative w-12 max-w-full aspect-square rounded-full grid place-items-center focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-400'
-
-  /* 
-    ------------------------------
-    Note that we have moved our `Status` type and styles in this file,
-    since we no longed need to access them in other places.
-    ------------------------------
-  */
 
   type Status = 'DISABLED' | 'SELECTED' | 'NO_VACANCY' | 'TODAY_NO_VACANCY' | 'VACANCY'
 
@@ -55,34 +37,12 @@ export default function CalendarDay({ state, date, bookingAvailabilities }) {
     VACANCY: 'bg-indigo-100 text-indigo-600 font-bold hover:bg-indigo-200',
   }
 
-  /* 
-    ------------------------------
-    ## CHALLENGE ##
-    1. Delete the legacy `dynamicClasses` styles object below.
-    WARNING: this will break the page 💥
-
-    You'll also need to delete all references to the `dynamicClasses` 
-    object in the `className` attribute of the returned JSX.
-    ------------------------------
-  */
-  const dynamicClasses = {
-    disabled: 'text-slate-300 pointer-events-none',
-    selected: 'bg-indigo-600 text-white font-bold bg-stripes',
-    candidate: 'text-slate-900 hover:bg-slate-100',
-    today: 'text-indigo-600 font-bold hover:bg-slate-100',
-    hasAvailability: 'bg-indigo-100 text-indigo-600 font-bold hover:bg-indigo-200',
-  }
-  /* 
-    ------------------------------
-     ## CHALLENGE ##
-    2. Write the logic inside the `getStatus` function below,  
-    so that it returns the correct availability status 
-    based on the `Status` type.
-    ------------------------------
-  */
   type GetStatus = () => Status
   const getStatus: GetStatus = function () {
-    // TODO: Return the correct availability status
+    if (isDisabled) return 'DISABLED'
+    if (isSelected) return 'SELECTED'
+    if (hasAvailability) return 'VACANCY'
+    return isCurrentDay ? 'TODAY_NO_VACANCY' : 'NO_VACANCY'
   }
 
   // ------------------------------
@@ -95,30 +55,18 @@ export default function CalendarDay({ state, date, bookingAvailabilities }) {
         {...buttonProps}
         ref={ref}
         hidden={isOutsideVisibleRange}
-        /*
-          ------------------------------
-          ## CHALLENGE ##
-          3. Add the correct `statusClasses` styles to the `className` 
-          attribute, using the `getStatus` function you've updated.
-          ------------------------------
-        */
         className={cx(
           baseClasses,
-          /* 
-            ## CHALLENGE ##
-            1b. All the styles below can go away.Instead of deleting them, 
-            comment them out so you can compare the "before/after"
-            in terms of complexity.
-          */
-          isSelected && dynamicClasses.selected,
-          isDisabled && dynamicClasses.disabled,
-          isCurrentDay && !isSelected && dynamicClasses.today,
-          hasAvailability && !isDisabled && !isSelected && dynamicClasses.hasAvailability,
-          !hasAvailability &&
-            !isCurrentDay &&
-            !isDisabled &&
-            !isSelected &&
-            dynamicClasses.candidate
+          statusClasses[getStatus()]
+          // isSelected && dynamicClasses.selected,
+          // isDisabled && dynamicClasses.disabled,
+          // isCurrentDay && !isSelected && dynamicClasses.today,
+          // hasAvailability && !isDisabled && !isSelected && dynamicClasses.hasAvailability,
+          // !hasAvailability &&
+          //   !isCurrentDay &&
+          //   !isDisabled &&
+          //   !isSelected &&
+          //   dynamicClasses.candidate
         )}
       >
         <span>{formattedDate}</span>
