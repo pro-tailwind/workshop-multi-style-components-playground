934861a21b5a69354d6efb85d7eaadb7530af8ef
Simon Vrachliotis <simon@simonswiss.com>
2022-09-29 10:55:54 +1000

    Button Props

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,41 +2,36 @@ import React from 'react'
 import { cx } from '../utils'
 
 // ------------------------------
+// Prop types
+// ------------------------------
+type ButtonProps = {
+  size?: 'small' | 'medium' | 'large'
+  impact?: 'bold' | 'light' | 'none'
+  shape?: 'square' | 'rounded' | 'pill'
+}
+
+// ------------------------------
 // Tailwind Classes lookup directory
 // ------------------------------
 const baseClasses =
   'font-semibold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 active:translate-y-px disabled:pointer-events-none disabled:opacity-50'
 
-const impactClasses = {
+const impactClasses: Record<ButtonProps['impact'], string> = {
   bold: 'bg-indigo-500 text-white shadow-md hover:bg-indigo-600',
   light: 'bg-indigo-100 text-indigo-700 hover:bg-indigo-200',
   none: 'bg-transparent text-indigo-700 hover:bg-indigo-50',
 }
 
-/*
-  ------------------------------
-  1. Populate the `sizeClasses` and `shapeClasses` lookup objects below, 
-  following the same approach we used for the `impactClasses` object.
-  ------------------------------
-*/
-const sizeClasses = {
+const sizeClasses: Record<ButtonProps['size'], string> = {
   small: 'px-3 py-1 text-sm',
   medium: 'px-5 py-2',
   large: 'px-7 py-2.5 text-lg',
 }
-const shapeClasses = {
+
+const shapeClasses: Record<ButtonProps['shape'], string> = {
   square: '',
   rounded: 'rounded',
   pill: 'rounded-full',
-}
-
-// ------------------------------
-// Possible prop values for `size`, `impact` and `shape`
-// ------------------------------
-interface ButtonProps extends React.ComponentProps<'button'> {
-  size?: 'small' | 'medium' | 'large'
-  impact?: 'bold' | 'light' | 'none'
-  shape?: 'square' | 'rounded' | 'pill'
 }
 
 // ------------------------------
@@ -47,16 +42,10 @@ const Button = ({
   impact = 'bold',
   shape = 'rounded',
   ...restProps
-}: ButtonProps) => {
+}: ButtonProps & React.ComponentProps<'button'>) => {
   return (
     <button
       {...restProps}
-      /* 
-        ------------------------------
-        2. Add the appropriate `sizeClasses` and `shapeClasses` values to the 
-        className attribute below.
-        ------------------------------
-      */
       className={cx(baseClasses, impactClasses[impact], sizeClasses[size], shapeClasses[shape])}
     />
   )
