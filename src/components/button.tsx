f026992a2d24c51866850959ff631fe41b118f70
Simon Vrachliotis <simon@simonswiss.com>
2022-09-29 17:21:54 +1000

    Step 5 instead of only solution

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,6 +8,10 @@ type ButtonProps = {
   size?: 'small' | 'medium' | 'large'
   impact?: 'bold' | 'light' | 'none'
   shape?: 'square' | 'rounded' | 'pill'
+  /*
+    Surprise! We've got an extra prop type now
+  */
+  tone?: 'default' | 'danger' | 'success'
 }
 
 // ------------------------------
@@ -16,7 +20,24 @@ type ButtonProps = {
 const baseClasses =
   'font-semibold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 active:translate-y-px disabled:pointer-events-none disabled:opacity-50'
 
-const impactClasses: Record<ButtonProps['impact'], string> = {
+/* 
+  The new `tone` prop only impacts the button colors, which are
+  already being handled within the `impactClasses` object.
+
+  Notice that the type signature of the `impactClasses` object
+  has now changed: it contains a **nested Record**,
+  because the `tone` prop affects what color
+  classes are being applied to the button.
+
+  Update the `impactClasses` structure to satisfy its defined type.
+
+  Here are the colors that each tone should take:
+
+  1. 'default': same as current, keep the `indigo` color
+  2. 'danger': replace `indigo` with `red` (keep the same number shades)
+  3. 'success': replace `indigo` with `green` (keep the same number shades)
+*/
+const impactClasses: Record<ButtonProps['tone'], Record<ButtonProps['impact'], string>> = {
   bold: 'bg-indigo-500 text-white shadow-md hover:bg-indigo-600',
   light: 'bg-indigo-100 text-indigo-700 hover:bg-indigo-200',
   none: 'bg-transparent text-indigo-700 hover:bg-indigo-50',
