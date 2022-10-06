47bf5acbad4785c85d32ec33b48d2239c5f20a53
Simon Vrachliotis <simon@simonswiss.com>
2022-10-06 17:59:55 +1100

    Rename keys instead of copying classes over

--- src/components/calendar/availability-status.ts
+++ src/components/calendar/availability-status.ts
@@ -12,32 +12,14 @@ export type Status = 'DISABLED' | 'SELECTED' | 'NO_VACANCY' | 'TODAY_NO_VACANCY'
 // Styles lookup dictionary
 // ------------------------------
 
-/* 
-  Let's start by listing all the classes that are used
-  across all calendar day statuses:
-*/
 export const baseClasses =
   'relative w-12 max-w-full aspect-square rounded-full grid place-items-center focus:outline-none focus:ring focus:ring-offset-1 focus:ring-indigo-400'
 
 /* 
-  TODO: Populate the `statusClasses` object below with the 
-  correct keys (statuses) and values (class strings)
-  to make both status tables look identical.
-
-  See the `dynamicClasses` object further down for reference!
+  TODO: Rename the `statusClasses` keys below to match the
+  finite list of Status values we have just established.
 */
-export const statusClasses: Record<Status, string> = {}
-
-// ------------------------------
-// `dynamicClasses` reference
-// ------------------------------
-/*
-  For your convenience, here's a copy of are the dynamic styles used 
-  in the calendar code at the beginning of this refactoring.
-  You can see those around line 30 of this file:
-  `src/components/reference/calendar-day.tsx`
-*/
-const dynamicClasses = {
+export const statusClasses: Record<Status, string> = {
   disabled: 'text-slate-300 pointer-events-none',
   selected: 'bg-indigo-600 text-white font-bold bg-stripes',
   candidate: 'text-slate-900 hover:bg-slate-100',
